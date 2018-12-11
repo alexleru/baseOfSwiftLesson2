9f4735247e71a22af72defe0038ef982081367e7
Alexleru <alexeymoblive@gmail.com>
2018-12-11 20:42:47 +0300

    создание проекта

---
+++ baseOfSwiftLesson2/baseOfSwiftLesson2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  baseOfSwiftLesson2
+//
+//  Created by Darya Bodaykina on 11.12.2018.
+//  Copyright © 2018 alexleru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
